4ab4767245d9bb18858dfeb0e2aaf5eb509752d0
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-10-14 06:55:07 +0300

    FIX: fixed client

--- client.py
+++ client.py
@@ -1,46 +1,13 @@
-# ************** Standart module *********************
-from datetime import datetime
-# ************** Standart module end *****************
+import websockets
+import asyncio
 
 
-# ************** External module *********************
-from fastapi import FastAPI
-from fastapi import Request
-from fastapi import WebSocket
-from starlette.websockets import WebSocketDisconnect
-# ************** External module end *****************
+async def sender(uri):
+    async with websockets.connect(uri) as websocket:
+        message = input("Input message")
+        await websocket.send(message)
+        reciev = await websocket.recv()
+        print(reciev)
 
 
-# Server instance creation
-app = FastAPI()
-
-
-# Save clients session
-# TODO: Нужно подумать как их компактно хранить
-clients = []
-
-
-# Chat websocket
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    clients.append(websocket)
-    while True:
-        try:
-            data = await websocket.receive_json()
-            client = controller.ProtocolMethods(data)
-            await websocket.send_json(client.get_response(), mode='binary')
-        except WebSocketDisconnect as error:
-            clients.remove(websocket)
-            break
-        except RuntimeError as error:
-            clients.remove(websocket)
-            break
-        else:
-            if websocket.client_state.value == 0:
-                await websocket.close(code=1000)
-                clients.remove(websocket)
-
-
-if __name__ == "__main__":
-    app
+asyncio.get_event_loop().run_until_complete(sender("ws://35.228.157.42:8000/ws"))
