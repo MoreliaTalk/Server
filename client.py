257e7c9ff88f4b99e06f9607d12feca75089755d
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-10-17 00:39:59 +0300

    message serialized to json

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import websockets
 import asyncio
+import json
 
 
 async def sender(uri):
     async with websockets.connect(uri) as websocket:
-        message = input("Input message")
-        await websocket.send(message)
+        message = input("Input message:\n")
+        await websocket.send(json.dumps(message))
         reciev = await websocket.recv()
         print(reciev)
 
 
-asyncio.get_event_loop().run_until_complete(sender("ws://35.228.157.42:8000/ws"))
+#asyncio.get_event_loop().run_until_complete(sender("ws://35.228.157.42:8000/ws"))
