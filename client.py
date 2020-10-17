94a91b3c99b937e7cdd6e8ec1300d1e34ef960b0
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-10-17 11:31:34 +0300

    added exception ConnectionClosedOK and hard-stop eventloop

--- client.py
+++ client.py
@@ -9,14 +9,17 @@ URI = "ws://35.228.157.42:8000/ws"
 async def sender(uri):
     while True:
         async with websockets.connect(uri) as websocket:
-            inputs = input("Input message:\n")
+            inputs = input("Input message (Enter to exit):\n")
             if inputs == '':
-                break
+                # hard stop enventloop
+                asyncio.get_event_loop().stop()
             else:
                 message = json.dumps(inputs)
                 await websocket.send(message)
-                reciev = await websocket.recv()
-                print(reciev)
+                try:
+                    await websocket.recv()
+                except websockets.exceptions.ConnectionClosedOK as error:
+                    print("Connections close: ", error)
 
 
 asyncio.get_event_loop().run_until_complete(sender(URI))
